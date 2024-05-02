545e7528b7d1db6db04d3303b12ae6ccc5001c8a
GITA <jgitanjali592@gmail.com>
2024-05-02 11:53:06 +0530

    my portfolio

---
+++ src/data.jsx
@@ -0,0 +1,446 @@
+import {
+  FaHome,
+  FaUser,
+  FaFolderOpen,
+  FaEnvelopeOpen,
+  FaBriefcase,
+  FaGraduationCap,
+  FaCode,
+} from 'react-icons/fa';
+import { FiFileText, FiUser, FiExternalLink } from 'react-icons/fi';
+
+import Work1 from './assets/project-1.png';
+import Work2 from './assets/project-2.png';
+import Work3 from './assets/project-3.png';
+import Work4 from './assets/project-4.png';
+import Work5 from './assets/project-5.png';
+import Work6 from './assets/project-6.png';
+
+import Theme1 from './assets/purple.png';
+import Theme2 from './assets/red.png';
+import Theme3 from './assets/blueviolet.png';
+import Theme4 from './assets/blue.png';
+import Theme5 from './assets/goldenrod.png';
+import Theme6 from './assets/magenta.png';
+import Theme7 from './assets/yellowgreen.png';
+import Theme8 from './assets/orange.png';
+import Theme9 from './assets/green.png';
+import Theme10 from './assets/yellow.png';
+
+export const links = [
+  {
+    id: 1,
+    name: 'Home',
+    icon: <FaHome className='nav__icon' />,
+    path: '/',
+  },
+
+  {
+    id: 2,
+    name: 'About',
+    icon: <FaUser className='nav__icon' />,
+    path: '/about',
+  },
+
+  {
+    id: 3,
+    name: 'Portfolio',
+    icon: <FaFolderOpen className='nav__icon' />,
+    path: '/portfolio',
+  },
+
+  {
+    id: 4,
+    name: 'Contact',
+    icon: <FaEnvelopeOpen className='nav__icon' />,
+    path: '/contact',
+  },
+];
+
+export const personalInfo = [
+  {
+    id: 1,
+    title: 'First Name : ',
+    description: 'Gitanjalee',
+  },
+
+  {
+    id: 2,
+    title: 'Last Name : ',
+    description: 'Jena',
+  },
+
+  {
+    id: 3,
+    title: 'Age : ',
+    description: '22 Years',
+  },
+
+  {
+    id: 4,
+    title: 'Nationality : ',
+    description: 'Hindu',
+  },
+
+
+
+  {
+    id: 5,
+    title: 'Address : ',
+    description: 'Bhubaneswar',
+  },
+
+  {
+    id: 6,
+    title: 'Phone : ',
+    description: '+919861548880',
+  },
+
+  {
+    id: 7,
+    title: 'Email : ',
+    description: 'jgitanjali592@gmail.com',
+  },
+
+  {
+    id: 8,
+    title: 'LinkedIn : ',
+    description: 'https://www.linkedin.com/in/gitanjali-jena-337376229/',
+  } ,
+
+  {
+    id: 9,
+    title: 'Github: ',
+    description: 'https://github.com/DEV-Gitanjali',
+  },
+
+
+  {
+    id: 10,
+    title: 'Langages : ',
+    description: 'Hindi', 'English',
+  },
+];
+
+export const stats = [
+  {
+    id: 1,
+    title: 'Fresher',
+  },
+
+  {
+    id: 2,
+    no: '50+',
+    title: 'Completed  HandsOn <br /> Projects',
+  },
+
+];
+
+export const resume = [
+
+  {
+    id: 1,
+    category: 'education',
+    icon: <FaGraduationCap />,
+    year: '2023',
+    title: 'Engineering Degree <span> Gandhi Institute of Excellent Technocrate</span>',
+    desc: 'from Computer Science Engineering(CSE)',
+  },
+
+  {
+    id: 5,
+    category: 'education',
+    icon: <FaGraduationCap />,
+    year: '2019',
+    title: '+2 science <span> Chandbali Junior College </span>',
+    
+  },
+
+  {
+    id: 6,
+    category: 'education',
+    icon: <FaGraduationCap />,
+    year: '2009',
+    title: 'Bachelor Degree <span> Durga Charan High School </span>',
+    
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    title: 'Html',
+    percentage: '80',
+  },
+
+  {
+    id: 2,
+    title: 'Javascript',
+    percentage: '75',
+  },
+
+  {
+    id: 3,
+    title: 'Css',
+    percentage: '70',
+  },
+
+  {
+    id: 4,
+    title: 'Tailwind Css',
+    percentage: '70',
+  },
+
+  {
+    id: 5,
+    title: 'React.js',
+    percentage: '70',
+  },
+
+  {
+    id: 6,
+    title: 'Jquery',
+    percentage: '50',
+  },
+
+  {
+    id: 7,
+    title: 'Bootsrap',
+    percentage: '65',
+  },
+
+  {
+    id: 8,
+    title: 'GSAP',
+    percentage: '45',
+
+  },
+];
+
+export const portfolio = [
+  {
+    id: 1,
+    img: Work1,
+    title: ' Movie App',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Photo',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Photoshop',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    img: Work2,
+    title: 'Website Design',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Website',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'React JS',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 3,
+    img: Work3,
+    title: 'Video Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Video',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Premium',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    img: Work4,
+    title: 'Video Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Video',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Premium',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 5,
+    img: Work5,
+    title: 'Landing Page',
+    details: [
+      {
+        title: 'Project : ',
+        desc: 'Website',
+      },
+      {
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        title: 'Language : ',
+        desc: 'React JS, Node JS',
+      },
+      {
+        title: 'Preview : ',
+        desc: 'www.dribble.com',
+      },
+    ],
+  },
+
+  {
+    id: 6,
+    img: Work6,
+    title: 'Photo Editing',
+    details: [
+      {
+        icon: <FiFileText />,
+        title: 'Project : ',
+        desc: 'Photo',
+      },
+      {
+        icon: <FiUser />,
+        title: 'Client : ',
+        desc: 'Dribble',
+      },
+      {
+        icon: <FaCode />,
+        title: 'Language : ',
+        desc: 'Adobe Photoshop',
+      },
+      {
+        icon: <FiExternalLink />,
+        title: 'Preview : ',
+        desc: 'www.dibble.com',
+      },
+    ],
+  },
+];
+
+export const themes = [
+  {
+    id: 1,
+    img: Theme1,
+    color: 'hsl(252, 35%, 51%)',
+  },
+
+  {
+    id: 2,
+    img: Theme2,
+    color: 'hsl(4, 93%, 54%)',
+  },
+
+  {
+    id: 3,
+    img: Theme3,
+    color: 'hsl(271, 76%, 53%)',
+  },
+
+  {
+    id: 4,
+    img: Theme4,
+    color: 'hsl(225, 73%, 57%)',
+  },
+
+  {
+    id: 5,
+    img: Theme5,
+    color: 'hsl(43, 74%, 49%)',
+  },
+
+  {
+    id: 6,
+    img: Theme6,
+    color: 'hsl(339, 81%, 66%)',
+  },
+
+  {
+    id: 7,
+    img: Theme7,
+    color: 'hsl(80, 61%, 50%)',
+  },
+
+  {
+    id: 8,
+    img: Theme8,
+    color: 'hsl(19, 96%, 52%)',
+  },
+
+  {
+    id: 9,
+    img: Theme9,
+    color: 'hsl(88, 65%, 43%)',
+  },
+
+  {
+    id: 10,
+    img: Theme10,
+    color: 'hsl(42, 100%, 50%)',
+  },
+];
