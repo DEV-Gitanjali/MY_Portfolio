29bc42ba6d1bd6f44f79441c40c765ea1c920714
GITA <jgitanjali592@gmail.com>
2024-05-04 23:04:24 +0530

    this  feature for portfolio item

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+// import React from 'react'
+import { portfolio} from '../../data';
+import PortfolioItem from "../../components/PortfolioItem";
+import './portfolio.css';
 
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
-    </div>
+    <section className="Portfolio section">
+      <h2 className="section__title">my<span>Portfolio</span></h2>
+      <div className="portfolio__container container grid">
+          {portfolio.map((item)=>{
+             return <PortfolioItem key={item.id}  {...item}/>
+          })}
+
+      </div>
+    </section>
   )
 }
 
-export default Portfolio
+export default Portfolio;
