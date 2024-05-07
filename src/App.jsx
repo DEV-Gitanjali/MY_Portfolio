5d80e98288aed02985351e2644c028d69009a2cd
GITA <jgitanjali592@gmail.com>
2024-05-07 23:14:35 +0530

    update my resume

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
          <Navbar />
-         <Themes/>
+         <Thegmes/>
         <Routes>
           
           <Route index element={<Home />} />
